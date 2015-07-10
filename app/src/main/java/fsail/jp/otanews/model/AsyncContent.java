66b7acf2c253ce40c52d4d46edc2a287ba08abb6
kakizaki2 <s-kakizaki.basicinc.jp>
2015-07-10 11:34:55 +0900

    一旦非同期でQiitaを取得出来るようにした

---
+++ app/src/main/java/fsail/jp/otanews/model/AsyncContent.java
@@ -0,0 +1,49 @@
+package fsail.jp.otanews.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kakizaki_shoichi on 2015/07/10.
+ */
+public class AsyncContent {
+
+    private boolean mSuccess;
+    private ArrayList<String> mTitle = new ArrayList<String>();
+    private ArrayList<String> mUri = new ArrayList<String>();
+
+    // JSONArray用のコンストラクタ
+    public AsyncContent(boolean success, JSONArray response) {
+        mSuccess = success;
+        createStatus(response);
+    }
+
+    public boolean isSuccess(){
+        return mSuccess;
+    }
+
+    public ArrayList title(){
+        return mTitle;
+    }
+
+    public ArrayList uri(){
+        return mUri;
+    }
+
+    // 必要なデータを生成する
+    private void createStatus(JSONArray response){
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                JSONObject qiita = (JSONObject) response.get(i);
+                mTitle.add(qiita.getString("title"));
+                mUri.add(qiita.getString("url"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/fsail/jp/otanews/model/CommunicationManager.java
@@ -0,0 +1,44 @@
+package fsail.jp.otanews.model;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+
+import de.greenrobot.event.EventBus;
+
+/**
+ * Created by kakizaki_shoichi on 2015/07/10.
+ */
+
+public class CommunicationManager {
+
+    private RequestQueue mQueue;
+    private JSONArray mResponse;
+    private final EventBus eventBus;
+
+    public CommunicationManager(){
+        eventBus = EventBus.getDefault();
+    }
+
+    public void getQiita(Context context) {
+        // Volley でリクエスト
+        mQueue = Volley.newRequestQueue(context);
+        String url = "https://qiita.com/api/v2/items";
+        mQueue.add(new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("Qiita","responzse : " + response);
+                        mResponse = response;
+                        // 非同期でeventBusにpostする
+                        eventBus.post(new AsyncContent(true, mResponse));
+                    }
+                }, null));
+
+    }
+
+}
